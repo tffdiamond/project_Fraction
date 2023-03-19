b86e5147a74a461e5cfbada7f19ce0691db5fafd
Lou <tffdiamond@gmail.com>
2023-03-19 22:47:34 +0800

    change

--- MixedFraction.java
+++ MixedFraction.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package project;
 
 class MixedFraction extends Fraction {
@@ -66,73 +65,4 @@ class MixedFraction extends Fraction {
     public String toString() {
         return "";
     }
-=======
-package project;
-
-class MixedFraction extends Fraction {
-    private int whole;
-
-    MixedFraction() {
-    }
-
-    MixedFraction(int whole, Fraction fraction) {
-        super(fraction);
-    }
-
-    MixedFraction(int whole, int numerator, int denominator) {
-        super(numerator, denominator);
-
-        // Formula for changing an improper fraction to mixed fraction
-        if (numerator > denominator) {
-            numerator = numerator % denominator;
-            whole = numerator / denominator;
-            setWholePart(whole);
-        }
-
-        new Fraction(numerator, denominator);
-    }
-
-    MixedFraction(Fraction fraction) {
-
-    }
-
-    public void setWholePart(int whole) {
-        this.whole = whole;
-    }
-
-    public void setFractionPart(Fraction fraction) {
-    }
-
-    public int getWhole() {
-        return whole;
-    }
-
-    public Fraction getFractionPart() {
-        return this;
-    }
-
-    public Fraction toFraction(){
-        return this;
-    }
-
-//    public MixedFraction add(MixedFraction other){
-//
-//    }
-//
-//    public MixedFraction sub(MixedFraction other){
-//
-//    }
-//
-//    public MixedFraction multiply(MixedFraction other){
-//
-//    }
-//
-//    public MixedFraction divide(MixedFraction other){
-//
-//    }
-
-    public String toString() {
-        return "";
-    }
->>>>>>> 12b86760da1eb34330e9301d6b5cef099c5b5331
 }
